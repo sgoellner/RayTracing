b8d67d1d1f8d76a625a7bbe44aa5d9c02a80f5d3
sgoellner <goellner@mailbox.org>
2019-11-15 14:15:16 +0100

    Typo

--- raytracing.py
+++ raytracing.py
@@ -8,10 +8,6 @@ import sys
 # Lade optisches System
 table = rt.loadOptSystem(sys.argv[1])
 
-# Surfaces berechnen
-ray = rt.calcYNU(table, [0, 0, 0.07])
-ray2 = rt.calcYNU(table, [0, table.loc[0,'z'], 0])
-
 # Eintrittspupille berechnen
 ep = rt.calcEP(table)
 print("dEP = {} mm".format(round(ep[0],2)))
@@ -19,9 +15,35 @@ print("hEP = {} mm".format(round(ep[1],2)))
 
 # Austrittspupille berechnen 
 ap = rt.calcAP(table)
-print("dEP = {} mm".format(round(ap[0],2)))
-print("hEP = {} mm".format(round(ap[1],2)))
+print("dAP = {} mm".format(round(ap[0],2)))
+print("hAP = {} mm".format(round(ap[1],2)))
+
+# bfl berechnen 
+bfl = rt.calcBFL(table)
+print("bfl = {} mm".format(round(bfl,2)))
+
+# ffl berechnen 
+ffl = rt.calcFFL(table)
+print("ffl = {} mm".format(round(ffl,2)))
  
+# efl berechnen 
+efl = rt.calcEFL(table)
+print("efl = {} mm".format(round(efl,2)))
+
+# Bildposition berechnen und in table schreiben
+dImage = rt.calcDImage(table)
+table.loc[len(table)-1, 'x'] += dImage
+print("dI = {} mm".format(round(table.loc[len(table)-1, 'x'], 2)))
+
+
+# Surfaces berechnen
+uMR = ep[1]/(table.loc[1].x-ep[0])
+uCR = -table.loc[0].z/(table.loc[1].x-ep[0])
+marginalRay = rt.calcYNU(table, [0, 0, uMR])
+chiefRay = rt.calcYNU(table, [0, table.loc[0,'z'], uCR]) 
+
+# Bildhöhe in table schreiben
+table.loc[len(table)-1, 'z'] = chiefRay[-1].y
 
 # Darstellung des Strahlenganges
-rt.plotOptSystem(table, [ray, ray2])
+rt.plotOptSystem(table, [marginalRay, chiefRay])
